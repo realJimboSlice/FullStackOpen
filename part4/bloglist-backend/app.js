366b476a4d2bcf088c000647b6037398ec158310
realJimboSlice <jbarbour95@hotmail.com>
2026-04-05 18:05:01 +0200

    Part 4.2 step 2 done

---
+++ part4/bloglist-backend/app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to MONGO')
+  })
+  .catch((error) => {
+    logger.error('Problem connecting to MONGO', error.message)
+  })
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.errorHandler)
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
